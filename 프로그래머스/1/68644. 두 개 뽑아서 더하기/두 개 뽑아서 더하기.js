c1d9d0ca8b5e1b1127a00ca5fb80d35ba62b1832
yoonjiKwon <kyjkbw@gmail.com>
2025-04-09 09:46:59 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.55 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,9 +1,11 @@
 function solution(numbers) {
-const result = new Set();
+let arr = []
     
 for(let i = 0 ; i < numbers.length ; i++){
     for(let j = i + 1 ; j < numbers.length ; j++){
-        result.add(numbers[i] + numbers[j])
+        arr.push(numbers[i] + numbers[j])
     }
-}  return [...result].sort((a, b) => a - b)
+}
+return [...new Set(arr)].sort((a, b)=> a - b)
+
 }
